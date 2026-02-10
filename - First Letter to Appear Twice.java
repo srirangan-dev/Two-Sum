11f5e864916fc456568973c81fec5cecc7dd81e8
srirangan <srirangan0603@gmail.com>
2026-02-10 13:54:29 +0530

    Create  - First Letter to Appear Twice.java

---
+++ - First Letter to Appear Twice.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+    
+    HashSet<Character> seen=new HashSet<>();
+
+    for(char c: s.toCharArray())
+    {
+        if(seen.contains(c)){
+          return c;
+        }
+        else{
+
+seen.add(c);
+        }
+    }
+
+return 0;
+    
+        
+    }
+}
